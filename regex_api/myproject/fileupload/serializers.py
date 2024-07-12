06311d80ff80688f333c078193bac4959379ac89
faizal <faizal@ailaysa.net>
2024-07-12 10:47:37 +0530

    delete

--- regex_api/myproject/fileupload/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import UploadFile
-
-class UploadFileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UploadFile
-        fields = ['file', 'uploaded_at']
